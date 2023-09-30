b329bfaf61dd6f9d5da9e34899267803cdb2a566
Ian Gelman <isgelman@wpi.edu>
2023-09-30 18:20:15 -0400

    add handler for update_pixel and add metadata

--- backend/app.py
+++ backend/app.py
@@ -1,19 +1,14 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
 from flask_cors import CORS
+from datetime import datetime
 
 # Creating a flask app and using it to instantiate a socket object
 app = Flask(__name__)
-app = CORS(app)
-socketio = SocketIO(app)
+CORS(app)
+socketio = SocketIO(app, cors_allowed_origins='*')
 
-# values['slider1'] and values['slider2'] store the current value of the sliders
-# This is done to prevent data loss on page reload by client.
-values = {
-    "slider1": 25,
-    "slider2": 0,
-}
-
+pixels = []
 
 # Handler for default flask route
 # Using jinja template to render html along with slider value as input
@@ -21,19 +16,27 @@ values = {
 def index():
     return render_template("index.html", **values)
 
-
 # Handler for a message recieved over 'connect' channel
 @socketio.on("connect")
 def test_connect():
     emit("after connect", {"data": "Lets dance"})
 
+@socketio.on("update_pixel")
+def update_pixel(message):
+    print(message)
+    pixel = {
+        'chunk': message['chunk'],
+        'x': message['x'],
+        'y': message['y'],
+        'color': message['color'],
+        #TODO: Figure out how to get IP
+        'metadata': {
+            'created_by': 'someone',
+            'created_at': datetime.now()
+        }
+    }
+    pixels.append(pixel)
 
-@socketio.on("Slider value changed")
-def value_changed(message):
-    values[message["who"]] = message["data"]
-    emit("update value", message, broadcast=True)
-
-
-# Notice how socketio.run takes care of app instantiation as well.
+#Instantiate socketio app
 if __name__ == "__main__":
     socketio.run(app, host="0.0.0.0")
