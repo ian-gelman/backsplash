c69d7f9ea8ff82cb803f4b5c9cd0432d664b31c6
Jack Michaud <jack@lomz.me>
2023-09-30 17:35:48 -0400

    feat: add flask_cors

--- backend/app.py
+++ backend/app.py
@@ -1,33 +1,39 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+from flask_cors import CORS
 
 # Creating a flask app and using it to instantiate a socket object
 app = Flask(__name__)
+app = CORS(app)
 socketio = SocketIO(app)
 
 # values['slider1'] and values['slider2'] store the current value of the sliders
 # This is done to prevent data loss on page reload by client.
 values = {
-    'slider1': 25,
-    'slider2': 0,
+    "slider1": 25,
+    "slider2": 0,
 }
+
 
 # Handler for default flask route
 # Using jinja template to render html along with slider value as input
-@app.route('/')
+@app.route("/")
 def index():
-    return render_template('index.html',**values)
+    return render_template("index.html", **values)
+
 
 # Handler for a message recieved over 'connect' channel
-@socketio.on('connect')
+@socketio.on("connect")
 def test_connect():
-    emit('after connect',  {'data':'Lets dance'})
+    emit("after connect", {"data": "Lets dance"})
 
-@socketio.on('Slider value changed')
+
+@socketio.on("Slider value changed")
 def value_changed(message):
-    values[message['who']] = message['data']
-    emit('update value', message, broadcast=True)
+    values[message["who"]] = message["data"]
+    emit("update value", message, broadcast=True)
+
 
 # Notice how socketio.run takes care of app instantiation as well.
-if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0')
+if __name__ == "__main__":
+    socketio.run(app, host="0.0.0.0")
